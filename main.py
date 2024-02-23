19bcacd5593211c8af79b29ca0457fdebec1ce59
TenkaGhoul <esteban.salingue@outlook.com>
2024-02-23 14:32:25 +0100

    Add Main Program

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+char_special = ["/", "?", "!", ":", "-", ";", "."]
+
+list_chiffre = []
+
+def init():
+    for i in range(10):
+        list_chiffre.append(i)
+
+password = []
+
+def make(length=15):
+    password = []
+    chiffre_str = [str(x) for x in list_chiffre]
+    
+    for j in range(length):
+        if j == 0:
+            password.append(random.choice(ALPHABET))
+        elif j == length - 1:
+            password.append(random.choice(char_special))
+        else:
+            if random.choice([True, False]):
+                password.append(random.choice(alphabet))
+            else:
+                password.append(random.choice(chiffre_str))
+
+    shuffled_password = list(password)
+    random.shuffle(shuffled_password)
+    print(''.join(shuffled_password))
+
+init()
+make()
